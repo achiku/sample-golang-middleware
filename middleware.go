a4cfe8e73df1c3b6b86572a8f086ef7323e36f78
Akira Chiku <akira.chiku@gmail.com>
2016-03-27 12:21:32 +0900

    Update

--- middleware.go
+++ middleware.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	}
+	return http.HandlerFunc(fn)
+}
+
+func recoverMiddleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %s\n", err)
+				w.Header().Set("Content-type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				res := map[string]string{"message": "error"}
+				json.NewEncoder(w).Encode(res)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
 
 // SingleHost middleware struct
 type SingleHost struct {
@@ -39,6 +67,15 @@ func SingleHost2(handler http.Handler, allowedHost string) http.Handler {
 			log.Printf("host [%s] is allowed host [%s]", host, allowedHost)
 			w.WriteHeader(http.StatusForbidden)
 		}
+	}
+	return http.HandlerFunc(fn)
+}
+
+// AppendMiddleware append msg
+func AppendMiddleware(handler http.Handler, msg string) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+		w.Write([]byte(msg))
 	}
 	return http.HandlerFunc(fn)
 }
--- server.go
+++ server.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+// AppHandler app handler
+type AppHandler func(http.ResponseWriter, *http.Request) (int, error)
+
+func (h AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	status, err := h(w, r)
+	if status == http.StatusInternalServerError {
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		res := map[string]string{"message": err.Error()}
+		json.NewEncoder(w).Encode(res)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) (int, error) {
 	fmt.Fprintf(w, "hello, world!")
+	return http.StatusOK, nil
+}
+
+func errorhello(w http.ResponseWriter, r *http.Request) (int, error) {
+	return http.StatusInternalServerError, errors.New("error")
 }
 
 func main() {
-	singleHostHello1 := NewSingleHost(http.HandlerFunc(hello), "somehost1")
-	singleHostHello2 := SingleHost2(http.HandlerFunc(hello), "somehost2")
+	singleHostHello1 := NewSingleHost(AppHandler(hello), "somehost1")
+	singleHostHello2 := SingleHost2(AppHandler(hello), "somehost2")
+	appendHello := AppendMiddleware(AppHandler(hello), " append this!")
 	mux := http.NewServeMux()
-	mux.Handle("/hello", http.HandlerFunc(hello))
+	mux.Handle("/hello", AppHandler(hello))
+	mux.Handle("/error/hello", AppHandler(errorhello))
 	mux.Handle("/singlehost1/hello", singleHostHello1)
 	mux.Handle("/singlehost2/hello", singleHostHello2)
+	mux.Handle("/append/hello", appendHello)
 	if err := http.ListenAndServe(":8011", mux); err != nil {
 		log.Fatal(err)
 	}
